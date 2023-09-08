acf3b73349cc114780aa8aac156d8786bc457cb3
girumel <girumel@email.com>
2023-09-08 10:41:57 +0300

    Added db connection

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,25 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import { connect } from 'mongoose'
+
+dotenv.config();
+
+const { MONGODB_URL, EXPRESS_PORT } = process.env
+
+const dbConnect = async () => {
+  try {
+    await connect(MONGODB_URL as string)
+    console.log('Connected to database')
+  }
+  catch (err) {
+    console.log(err)
+  }
+}
+
+dbConnect().catch(err => console.log(err))
 
 const app: express.Application = express()
-const port: number = 3000
+const port: number = parseInt(EXPRESS_PORT as string) || 3000
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
