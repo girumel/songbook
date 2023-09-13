e5a25ef7a24788a555c9059cfbaa5f7fe342f6ce
girumel <girumel@email.com>
2023-09-13 19:55:12 +0300

    Updated stats route

--- backend/routes/stats.ts
+++ backend/routes/stats.ts
@@ -28,4 +28,37 @@ stats.get('/total', async (_req: Request, res: Response) => {
     }
 })
 
+stats.get('/songs-by-artist/:artist', async (req: Request, res: Response) => {
+    try {
+        const songs: Song[] = await SongModel.find()
+        const songsByArtist = songs.filter(song => song.artist === req.params.artist)
+        res.status(200).json(songsByArtist.length)
+    }
+    catch (err) {
+        res.status(400).json(err)
+    }
+})
+
+stats.get('/songs-by-genre/:genre', async (req: Request, res: Response) => {
+    try {
+        const songs: Song[] = await SongModel.find()
+        const songsByGenre = songs.filter(song => song.genre === req.params.genre)
+        res.status(200).json(songsByGenre.length)
+    }
+    catch (err) {
+        res.status(400).json(err)
+    }
+})
+
+stats.get('/songs-by-album/:album', async (req: Request, res: Response) => {
+    try {
+        const songs: Song[] = await SongModel.find()
+        const songsByAlbum = songs.filter(song => song.album === req.params.album)
+        res.status(200).json(songsByAlbum.length)
+    }
+    catch (err) {
+        res.status(400).json(err)
+    }
+})
+
 export default stats
